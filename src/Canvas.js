e07510a98ed6108fbcf55eff04a46450e1605eae
DeskorED <rfpfywtdf29@gmail.com>
2022-10-20 15:02:38 +0200

    new method Clear, change logic in eventListeners

--- src/Canvas.js
+++ src/Canvas.js
@@ -5,53 +5,89 @@ class Canvas {
         this.lineArr = [
             {
                 x: 10,
-                y: 10
+                y: 10,
+                point: 'start'
             },
             {
                 x: 30,
-                y: 50
+                y: 50,
+                point: 'end'
             },
             {
                 x: 20,
-                y: 40
+                y: 40,
+                point: 'start'
             },
             {
                 x: 50,
-                y: 30
+                y: 30,
+                point: 'end'
             },
         ];
         this.Draw = this.Draw.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
+        this.onMouseClick = this.onMouseClick.bind(this);
+        this.Clear = this.Clear.bind(this);
+
     }
 
     onMouseClick(event) {
-        this.Draw(event.x, event.y, !this.start);
+        this.Draw(event.offsetX, event.offsetY, 'onclick');
 
     }
 
     onMouseMove(event) {
-
-        window.requestAnimationFrame(() => this.Draw(event.x, event.y, this.start));
+        window.requestAnimationFrame(() => this.Draw(event.offsetX, event.offsetY, 'onmousemove'));
     }
 
-    Draw(x, y, state) {
+    PreDraw() {
+        if (this.lineArr) {
+            return null;
+        }
         const ctx = this.cnc;
         ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
         ctx.strokeStyle = 'black';
+        const elemArr = this.lineArr;
+        for (let elem of elemArr) {
+            if (elem.point === "start") {
+                ctx.beginPath();
+                ctx.moveTo(elem.x, elem.y);
+            } else {
 
-        if(state != this.start){
-            this.start = state;
-             this.cnc.lineTo(x, y);
-        }
-        for (let i = 0; i < this.lineArr.length; i+=2) {
-            if(i+1 == this.lineArr.length){
-                break;
             }
-            ctx.beginPath();
-            ctx.moveTo(this.lineArr.keys()[i], this.lineArr.values()[i]);
-            ctx.lineTo(this.lineArr.keys()[i+1], this.lineArr.values()[i+1]);
-            ctx.closePath();
+        }
+        return ctx;
+    }
+
+    Draw(x, y, eventType) {
+        const ctx = this.PreDraw();
+        if (ctx) {
+            if (eventType === "onclick") {
+
+                if (this.start !== true) {
+                    ctx.beginPath();
+                    ctx.moveTo(x, y);
+                    this.lineArr.push({x: x, y: y, point: 'start'})
+                    this.start = !this.start;
+                } else {
+                    ctx.lineTo(x, y);
+                    ctx.closePath();
+                    this.lineArr.push({x: x, y: y, point: 'end'})
+                    this.start = !this.start;
+                }
+            } else {
+                if (this.start === true) {
+                    ctx.lineTo(x, y);
+                }
+            }
         }
     }
+
+    Clear() {
+        this.cnc.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        this.lineArr.clear();
+    }
+
 }
 
 export default Canvas;
--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ import Canvas from "./Canvas.js";
 
 const canvas = document.getElementById("test")
 if (canvas.getContext) {
+    const clearButton = document.getElementById("clear");
     const canvas2d = canvas.getContext('2d');
 
-
     global.addEventListener('resize', () => {
-        canvas2d.width = '1000px';
-        canvas2d.height = '800px';
+            canvas2d.width = '1000px';
+            canvas2d.height = '800px';
         }
     );
 
     let myCanvas = new Canvas(canvas2d);
     canvas.addEventListener('click', (event) => myCanvas.onMouseClick(event));
     canvas.addEventListener('mousemove', (event) => myCanvas.onMouseMove(event));
-    myCanvas.Draw();
+    myCanvas.PreDraw();
+    if (clearButton) {
+        clearButton.addEventListener('click', myCanvas.Clear);
+    }
 }
 
 
