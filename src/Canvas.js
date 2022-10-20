c320ca58fd8bb291172342ddb6abea97777d8084
DeskorED <rfpfywtdf29@gmail.com>
2022-10-20 17:03:18 +0200

    complete with draw and preDraw methods, right displaying

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,16 +1,19 @@
 class Canvas {
     constructor(cnc) {
-        this.cnc = cnc;
+        this.cnv = cnc;
+        this.cnc = cnc.getContext('2d');
+        cnc.width = 1000;
+        cnc.height = 1000;
         this.start = false;
         this.lineArr = [
             {
-                x: 10,
-                y: 10,
+                x: 100,
+                y: 50,
                 point: 'start'
             },
             {
                 x: 30,
-                y: 50,
+                y: 10,
                 point: 'end'
             },
             {
@@ -19,73 +22,93 @@ class Canvas {
                 point: 'start'
             },
             {
+                x: 100,
+                y: 10,
+                point: 'end'
+            },
+            {
                 x: 50,
-                y: 30,
+                y: 50,
+                point: 'start'
+            },
+            {
+                x: 200,
+                y: 100,
                 point: 'end'
             },
         ];
         this.Draw = this.Draw.bind(this);
+        this.PreDraw = this.PreDraw.bind(this);
+        this.Clear = this.Clear.bind(this);
         this.onMouseMove = this.onMouseMove.bind(this);
         this.onMouseClick = this.onMouseClick.bind(this);
-        this.Clear = this.Clear.bind(this);
-
     }
 
     onMouseClick(event) {
-        this.Draw(event.offsetX, event.offsetY, 'onclick');
+        let mouse = this.getMousePos(this.cnv, event);
+        this.Draw(mouse.x, mouse.y, 'onclick');
 
+    }
+
+    getMousePos(canvas, evt) {
+        let rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
     }
 
     onMouseMove(event) {
-        window.requestAnimationFrame(() => this.Draw(event.offsetX, event.offsetY, 'onmousemove'));
+        let mouse = this.getMousePos(this.cnv, event);
+        window.requestAnimationFrame(() => this.Draw(mouse.x, mouse.y, 'onmousemove'));
     }
 
     PreDraw() {
-        if (this.lineArr) {
+        if (!this.lineArr) {
             return null;
         }
-        const ctx = this.cnc;
-        ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        ctx.strokeStyle = 'black';
-        const elemArr = this.lineArr;
-        for (let elem of elemArr) {
+        this.cnc.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        this.cnc.strokeStyle = "black";
+        this.cnc.beginPath();
+        for (let elem of this.lineArr) {
             if (elem.point === "start") {
-                ctx.beginPath();
-                ctx.moveTo(elem.x, elem.y);
+
+                this.cnc.moveTo(elem.x, elem.y);
             } else {
+                this.cnc.lineTo(elem.x, elem.y)
 
             }
         }
-        return ctx;
+        this.cnc.stroke();
     }
 
     Draw(x, y, eventType) {
-        const ctx = this.PreDraw();
-        if (ctx) {
-            if (eventType === "onclick") {
+        this.cnc.beginPath();
+        this.PreDraw();
+        this.cnc.strokeStyle = "black";
+        if (eventType === "onclick") {
 
-                if (this.start !== true) {
-                    ctx.beginPath();
-                    ctx.moveTo(x, y);
-                    this.lineArr.push({x: x, y: y, point: 'start'})
-                    this.start = !this.start;
-                } else {
-                    ctx.lineTo(x, y);
-                    ctx.closePath();
-                    this.lineArr.push({x: x, y: y, point: 'end'})
-                    this.start = !this.start;
-                }
+            if (!this.start) {
+                this.cnc.moveTo(x, y);
+                this.lineArr.push({x: x, y: y, point: 'start'})
+                this.start = !this.start;
             } else {
-                if (this.start === true) {
-                    ctx.lineTo(x, y);
-                }
+                this.cnc.lineTo(x, y);
+                this.lineArr.push({x: x, y: y, point: 'end'})
+                this.start = !this.start;
+            }
+        } else {
+            if (this.start) {
+                this.cnc.lineTo(x, y);
             }
         }
+        this.cnc.stroke();
     }
 
     Clear() {
-        this.cnc.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        this.lineArr.clear();
+        this.cnc.clearRect(0, 0, this.cnv.width, this.cnv.height);
+        this.lineArr = [];
+        this.PreDraw();
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -2,23 +2,15 @@ import Canvas from "./Canvas.js";
 
 
 const canvas = document.getElementById("test")
-if (canvas.getContext) {
-    const clearButton = document.getElementById("clear");
-    const canvas2d = canvas.getContext('2d');
 
-    global.addEventListener('resize', () => {
-            canvas2d.width = '1000px';
-            canvas2d.height = '800px';
-        }
-    );
+const clearButton = document.getElementById("clear");
+let myCanvas = new Canvas(canvas);
+canvas.addEventListener('click', (event) => myCanvas.onMouseClick(event));
+canvas.addEventListener('mousemove', (event) => myCanvas.onMouseMove(event));
+myCanvas.PreDraw();
 
-    let myCanvas = new Canvas(canvas2d);
-    canvas.addEventListener('click', (event) => myCanvas.onMouseClick(event));
-    canvas.addEventListener('mousemove', (event) => myCanvas.onMouseMove(event));
-    myCanvas.PreDraw();
-    if (clearButton) {
-        clearButton.addEventListener('click', myCanvas.Clear);
-    }
-}
+clearButton.addEventListener('click', myCanvas.Clear);
+
+
 
 
